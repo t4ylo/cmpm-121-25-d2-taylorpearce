fdeb520b4b9e13a0f17c68b1b84a6ab00b86c651
t4ylo <thpearce@ucsc.edu>
2025-10-15 17:15:53 +0000

    Added mouseevents to allow player to draw and a clear button (Step 2 Complete)
    Co-authored-by: Wendy Zhang <wendyzh05@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "./style.css";
 
+type Cursor = { active: boolean; x: number; y: number };
+const cursor: Cursor = { active: false, x: 0, y: 0 };
+
 function createAppTitle(titleText: string): HTMLElement {
   const title = document.createElement("h1");
   title.textContent = titleText;
@@ -11,6 +14,7 @@ function createDrawingCanvas(width: number, height: number): HTMLCanvasElement {
   canvas.width = width;
   canvas.height = height;
   canvas.id = "drawingCanvas";
+  canvas.style.border = "1px solid #ccc";
   return canvas;
 }
 
@@ -21,9 +25,48 @@ function initUI(): void {
 
   const title = createAppTitle("Sticker Sketchpad");
   const canvas = createDrawingCanvas(256, 256);
+  const ctx = canvas.getContext("2d");
+  if (!ctx) throw new Error("Failed to get 2D context");
+
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
 
   app.appendChild(title);
   app.appendChild(canvas);
+
+  
+  canvas.addEventListener("mousedown", (e: MouseEvent) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  });
+
+  canvas.addEventListener("mouseup", () => {
+    cursor.active = false;
+  });
+
+  canvas.addEventListener("mousemove", (e: MouseEvent) => {
+    if (!cursor.active) return;
+
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  });
+
+  const clearButton = document.createElement("button");
+      clearButton.innerHTML = "clear";
+      document.body.append(clearButton);
+
+      clearButton.addEventListener("click", () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      });
+
 }
 
 initUI();
+
